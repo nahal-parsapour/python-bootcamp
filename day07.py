7de2d039b82e20a4875a49f535edcdd378eb3fbe
nahal parsapour <nahal.parsapour@yahoo.com>
2026-08-02 22:02:05 +0330

    Day 7 Updated

--- day07.py
+++ day07.py
@@ -5,14 +5,14 @@ print("Temperature Converter")
 mode = input("Convert from (C)elsius or (F)arenheit? ").lower()
 
 if mode == "c":
-    c = foat(input("Enter temperature in Celsius: ")
+    c = float(input("Enter temperature in Celsius: "))
     f = (c * 9/5) + 32
-    print(f"{c}°C = {f}°F")
+    print(f"{c}°C = {f:.2f}°F")
 
-elif mode == "f"
-    f = float(input("Enter temperature in Farenheit :")
+elif mode == "f":
+    f = float(input("Enter temperature in Farenheit :"))
     c = (f - 32) * 5/9
-    print("f{f}°F = {c}°C")
+    print(f"{f}°F = {c:.2f}°C")
 
 else:
     print("Invalid mode")
