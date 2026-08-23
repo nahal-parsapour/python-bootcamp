6e48f5781897bfbe00b1b05c28bc7f6f9bba2f2c
nahal parsapour <nahal.parsapour@gmail.com>
2026-08-23 21:05:39 +0330

    ...

---
+++ 12 Web Foundations/01git.py
@@ -0,0 +1,19 @@
+git --version
+git config
+git config --l
+git config list
+git config --global --get core.editor
+git git config --global user.name ("...")
+git git config --global user.email "...@gmail.com"
+config --global core.editor "code --wait"
+git config --global --edit
+git config --system --edit
+
+git init
+cd .git
+ls
+code config
+git status
+
+git add 12\ Web\ Foundations/
+git commit "..."
